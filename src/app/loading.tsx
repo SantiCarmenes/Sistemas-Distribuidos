e1c008273707e75c6f4a4271a8e225a2203e4920
Santiago Cármenes <santiagocarmenes@gmail.com>
2025-10-21 11:04:34 -0300

    Fix: Estructura corregida

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -11,7 +11,7 @@ export default function Loading() {
   return (
     <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
       {/* Repetimos el skeleton varias veces para simular la lista */}
-      {Array.from({ length: 10 }).map((_, index) => (
+      {Array.from({ length: 15 }).map((_, index) => (
         <SkeletonCard key={index} />
       ))}
     </div>
